6d4b5e5a5145d2e62c827d6c0c3ce446f64f3015
ATULYADAVpro <atul088793151@gmail.com>
2024-12-22 15:00:21 +0530

    cg

--- server/utils/db/db.js
+++ server/utils/db/db.js
@@ -6,10 +6,15 @@ import { DB_URL } from '../configs/index.js';
 // --------- Logic Part ----------
 const connectDb = async () => {
     try {
-        await mongoose.connect(DB_URL);
-        console.log('Database connected successFull.')
+        await mongoose.connect(DB_URL, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
+            family: 4, // Forces the use of IPv4 to avoid IPv6 issues
+        });
+        console.log('Database connected successfully.')
     } catch (error) {
-        console.log("database failed: ")
+        console.log("Database connection failed: ", error);
     }
 }
 
